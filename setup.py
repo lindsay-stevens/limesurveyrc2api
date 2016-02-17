feebff8c355e9e4bed5a2cc6edcb7f88f4ece766
lindsay stevens <lindsay.stevens.au@gmail.com>
2016-02-18 10:40:17 +1100

    chg: update setup.py for version 1
    yay

--- setup.py
+++ setup.py
@@ -2,13 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="limesurveyrc2api",
-    version="0.0.1",
+    version="1.0.0",
     description="LimeSurvey RC2 API Web Services Client",
+    url="https://github.com/lindsay-stevens-kirby/",
     author="Lindsay Stevens",
     author_email="lindsay.stevens.au@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    url="https://github.com/lindsay-stevens-kirby/limesurveyrc2api",
     license="MIT",
     install_requires=['requests'],
+    keywords="limesurvey api webservice client",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+    ],
 )
