ddb5468559291fe101fde4058c3c407b7bde6abf
ESchae <elke.schaechtele@web.de>
2018-01-04 19:27:07 +0100

    added import_survey and tests

--- limesurveyrc2api/_survey.py
+++ limesurveyrc2api/_survey.py
@@ -79,22 +79,40 @@ class _Survey(object):
             assert response_type is list
         return response
 
-    def copy_survey(self, survey_id_org, new_name):
-        # TODO: Make it work
-        """ RCP Routine to copy a survey.
+    def import_survey(self, path_to_import_survey, new_name=None,
+                      dest_survey_id=None):
+        """ Import a survey. Allowed formats: lss, csv, txt or lsa
         
         Parameters
-        :param survey_id_org: ID of the source survey.
-        :type survey_id_org: Integer
-        :param new_name: Name of the new survey.
+        :param path_to_import_survey: Path to survey as file to copy.
+        :type path_to_import_survey: String
+        :param new_name (optional): The optional new name of the survey
+                    Important! Seems only to work if lss file is given!
         :type new_name: String
+        :param dest_survey_id (optional): This is the new ID of the survey - 
+                          if already used a random one will be taken instead
+        :type dest_survey_id: Integer
         """
-        warnings.warn("This method is not working yet!", RuntimeWarning)
-        method = "copy_survey"
+        import_datatype = splitext(path_to_import_survey)[1][1:]
+        # TODO: Naming seems only to work with lss files - why?
+        if import_datatype == 'lss' and new_name:
+            warnings.warn("New naming seems only to work with lss files",
+                          RuntimeWarning)
+        # encode import data
+        with open(path_to_import_survey, 'rb') as f:
+            # import data must be a base 64 encoded string
+            import_data = b64encode(f.read())
+            # decoding needed because json.dumps() in method get of
+            # class LimeSurvey can not encode bytes
+            import_data = import_data.decode('ascii')
+
+        method = "import_survey"
         params = OrderedDict([
             ("sSessionKey", self.api.session_key),
-            ("iSurveyID_org", survey_id_org),
-            ("sNewname", new_name)
+            ("sImportData", import_data),
+            ("sImportDataType", import_datatype),
+            ("sNewSurveyName", new_name),
+            ("DestSurveyID", dest_survey_id)
         ])
         response = self.api.query(method=method, params=params)
         response_type = type(response)
@@ -102,14 +120,14 @@ class _Survey(object):
         if response_type is dict and "status" in response:
             status = response["status"]
             error_messages = [
-                "Copy failed",
+                "Error: ...",  # TODO: Unclear what might be returned here
+                "Invalid extension",
+                "No permission",
                 "Invalid session key"
             ]
             for message in error_messages:
                 if status == message:
-                    # return response instead of status, because of own error
-                    # message in case of "Copy failed" in response['error']
-                    raise LimeSurveyError(method, response)
+                    raise LimeSurveyError(method, status)
         else:
-            assert response_type is list
+            assert response_type is int  # the new survey id
         return response
