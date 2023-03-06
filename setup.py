a35860cf6013de5e6dc573df5f3f0dc648a9a296
lindsay stevens <lindsay.stevens.au@gmail.com>
2023-03-07 03:10:50 +1100

    dev: update packaging
    - remove .idea project files, not relevant
    - move prod deps into setup.py, dev deps into dev_requirements.pip
    - add install steps for general usage and dev
    - update LICENSE year
    - update target Python version to 3.9
    - simplify .gitignore
    - add github actions workflow to build dist files

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 from limesurveyrc2api import __version__
 
 setup(
     name="limesurveyrc2api",
     version=__version__,
     description="LimeSurvey RC2 API Web Services Client",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/lindsay-stevens",
     author="Lindsay Stevens",
     author_email="lindsay.stevens.au@gmail.com",
-    packages=["limesurveyrc2api"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
     test_suite="tests",
     include_package_data=True,
     license="MIT",
     install_requires=[
-        # see requirements.txt
+        "requests==2.28.2",
     ],
     keywords="limesurvey api webservice client",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.9",
     ],
 )
